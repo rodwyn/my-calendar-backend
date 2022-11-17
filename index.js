94102c3f896b652fbac6cfb026d83476f82d2068
Rodwyn Moreno <rodwynmoreno@gmail.com>
2022-11-17 16:03:44 -0600

    Set Express.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hola Pikachu');
+const express = require('express');
+
+// crear el servidor de express
+const app = express();
+
+// rutas
+app.get('/', (request, response) => {
+  response.json({
+    ok: true
+  });
+})
+
+// escuchar peticiones
+app.listen(4000, () => {
+  console.log(`Servidor corriendo en el puerto 4000`);
+});
