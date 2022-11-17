c4fc78c9a7de26883853ad41bc44eb1fa36e1867
Rodwyn Moreno <rodwynmoreno@gmail.com>
2022-11-17 16:21:45 -0600

    Added public folder.

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
+require('dotenv').config();
 
 // crear el servidor de express
 const app = express();
 
+// directorio público
+app.use( express.static('public'));
+
 // rutas
-app.get('/', (request, response) => {
-  response.json({
-    ok: true
-  });
-})
+// app.get('/', (request, response) => {
+//   response.json({
+//     ok: true
+//   });
+// })
 
 // escuchar peticiones
-app.listen(4000, () => {
-  console.log(`Servidor corriendo en el puerto 4000`);
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${ process.env.PORT }`);
 });
