c5caacbe44a42871e5c7cd834402fcc27e10c8d7
Rodwyn Moreno <rodwynmoreno@gmail.com>
2022-11-25 14:57:53 -0600

    Added create user behavior.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,23 @@
 const {response} = require('express');
-const { validationResult } = require('express-validator');
+const User = require('../models/User');
 
-const createUser = (req, res = response) => {
-  const { name, email, password } = req.body;
-
-  res.status(201).json({
-    ok: true,
-    msg: 'register',
-    name,
-    email,
-    password
-  });
+const createUser = async(req, res = response) => {
+  // const { name, email, password } = req.body;
+  try {
+    const user = new User(req.body);
+    await user.save();
+  
+    res.status(201).json({
+      ok: true,
+      msg: 'register'
+    });  
+  } catch (error) {
+   console.log(error); 
+   res.status(500).json({
+    ok: false,
+    msg: 'Error saving, contact administrator.'
+   });
+  }
 };
 
 const loginUser = (req, res = response) => {
