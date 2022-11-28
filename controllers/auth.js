9e655bcaf1ccb65bab67c59d96bcff89a9b22a44
Rodwyn Moreno <rodwynmoreno@gmail.com>
2022-11-28 16:36:34 -0600

    Generated JWT.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const {response} = require('express');
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const { generateJWT } = require('../helpers/jwt');
 
 const createUser = async(req, res = response) => {
   const { email, password } = req.body;
@@ -21,11 +22,14 @@ const createUser = async(req, res = response) => {
     user.password = bcrypt.hashSync(password, salt)
 
     await user.save();
+
+    const token = await generateJWT(user.id, user.name);
   
     res.status(201).json({
       ok: true,
       uid: user.id,
-      name: user.name
+      name: user.name,
+      token
     });  
   } catch (error) {
    console.log(error);
@@ -60,11 +64,13 @@ const loginUser = async(req, res = response) => {
     }
 
     //todo generate JWT
+    const token = await generateJWT(user.id, user.name);
 
     res.status(201).json({
       ok: true,
       uid: user.id,
-      name: user.name
+      name: user.name,
+      token
     });  
 
   } catch (error) {
