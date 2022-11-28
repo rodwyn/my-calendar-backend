5fece32e9620b38b6a6cb6ba085091cf9a9c7c01
Rodwyn Moreno <rodwynmoreno@gmail.com>
2022-11-28 12:33:45 -0600

    Added user validations.

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,14 +2,25 @@ const {response} = require('express');
 const User = require('../models/User');
 
 const createUser = async(req, res = response) => {
-  // const { name, email, password } = req.body;
+  const { email, password } = req.body;
+
   try {
-    const user = new User(req.body);
+    let user = await User.findOne({ email });
+    
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        msg: `E-mail ${ email } has already been registered.`
+      });
+    }
+
+    user = new User(req.body);
     await user.save();
   
     res.status(201).json({
       ok: true,
-      msg: 'register'
+      uid: user.id,
+      name: user.name
     });  
   } catch (error) {
    console.log(error); 
