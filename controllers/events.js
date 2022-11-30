c16701f9e0ae7b2c2a22bad0c8466b50a677c994
Rodwyn Moreno <rodwynmoreno@gmail.com>
2022-11-30 16:28:33 -0600

    Added save event on db behavior.

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const {response} = require('express');
+const Event = require('../models/Event');
 
 const getEvents = ( req, res = response ) => {
   res.status(201).json({
@@ -7,12 +8,24 @@ const getEvents = ( req, res = response ) => {
   });  
 };
 
-const createEvent = ( req, res = response ) => {
-  
-  res.status(201).json({
-    ok: true,
-    msg: 'createEvent'
-  });  
+const createEvent = async( req, res = response ) => {
+  const event = new Event(req.body);
+
+  try {
+    event.user = req.uid;
+    const savedEvent = await event.save();
+
+    res.json({
+      ok: true,
+      event: savedEvent
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Error creating event."
+    });
+  }
 };
 
 const updateEvent = ( req, res = response ) => {
